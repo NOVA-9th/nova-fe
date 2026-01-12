f481c21dc6bb2ea314a4a60ac3deeb54f48dbd9b
정찬원 <jcw0522@gachon.ac.kr>
2026-01-12 19:50:40 +0900

    ⚙️ chore: initial setting

---
+++ nova-app/eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettierConfig, // Prettier와 충돌하는 ESLint 규칙 비활성화
+
+  // Prettier를 ESLint 규칙으로 실행
+  {
+    plugins: prettier,
+  },
+
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
+
+export default eslintConfig;
---
+++ nova-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className='flex min-h-dvh items-center justify-center rounded-md px-4 text-xl font-semibold'>
+      React 19 베타 출시
+    </div>
+  );
+};
+
+export default page;
