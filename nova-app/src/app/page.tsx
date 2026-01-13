c30ca3b21a09a74b414932b5ca77bc99bbaff7c4
정찬원 <jcw0522@gachon.ac.kr>
2026-01-13 11:27:29 +0900

    ⚙️ chore: 레이어 유틸리티로 css 조건 분기

--- nova-app/src/app/page.tsx
+++ nova-app/src/app/page.tsx
@@ -1,8 +1,7 @@
 const page = () => {
   return (
     <div className='flex min-h-dvh flex-col items-center justify-center gap-5 rounded-md px-4 text-xl'>
-      <h1 className='bg-light-full rounded-step3 p-4'>텍스트 1</h1>
-      <h1 className='bg-slate-surface rounded-step3 font-hero p-4'>텍스트 1</h1>
+      안녕하세요
     </div>
   );
 };
