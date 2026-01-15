9a304112b4cb34c0fbf8d5ea651d5145fa486a16
jskim6335-plaid <jskim6335@plaid.ai.kr>
2026-01-16 05:21:16 +0900

    :lipstick: style: content 공용 컴포넌트 4가지 UI 작업 중. hover와 active 추가 필요

---
+++ nova-app/src/shared/ui/Content/DotBadge.tsx
@@ -0,0 +1,61 @@
+import { cva } from "class-variance-authority";
+
+const DotBadgeVariants = cva(
+    "flex items-center justify-center rounded-[1000px]",
+    {
+      variants: {
+        size: {
+          sm: "w-1 h-1",
+          md: "w-1.5 h-1.5",
+          lg: "w-2 h-2",
+        },
+        
+        variant: {
+          surface: "",
+          data: "",
+          accent: "",
+        },
+      },
+
+      compoundVariants: [
+        // surface + peak 조합
+        {
+          variant: "surface",
+          class: "bg-peak",
+        },
+        {
+          variant: "surface",
+          class: "bg-surface",
+        },
+        // accent + peak 조합
+        {
+          variant: "accent",
+          class: "bg-accent-peak",
+        },
+        {
+          variant: "accent",
+          class: "bg-accent-surface",
+        },
+        // data + peak 조합
+        {
+          variant: "data",
+          class: "bg-data-peak",
+        },
+        {
+          variant: "data",
+          class: "bg-data-surface",
+        },
+      ],
+    }
+);
+
+interface DotBadgeProps {
+  size: "sm" | "md" | "lg",
+  variant: "surface" | "accent" | "data" ,
+}
+
+export default function DotBadge({ size, variant } : DotBadgeProps) {
+    return (
+      <div className={DotBadgeVariants({ size: size, variant: variant })}/>
+    )
+}
