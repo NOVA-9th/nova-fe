37dfbb760005edbb8dfb7c21c9f2fc6fbd1001ec
JeonSuna <jsa020910@gmail.com>
2026-01-18 03:01:45 +0900

    ✅ test: Action Btn 컴포넌트 test

--- nova-app/src/app/(pages)/test/page.tsx
+++ nova-app/src/app/(pages)/test/page.tsx
@@ -1,20 +1,30 @@
 import { Button } from '@/shared/ui/action/button/Button';
-
+import { IconBtn } from '@/shared/ui/action/icon_button/IconBtn';
+import { TextBtn } from '@/shared/ui/action/text_button/TextBtn';
+import { ContentIcon } from '@/shared/ui/icon/Content';
 const testPage = () => {
   return (
     <div className='bg-base flex min-h-dvh flex-col items-center justify-center gap-5 rounded-md px-4 text-xl'>
       안녕하세요
-      <p className='text-footnote-key text-data-peak border-outline py-padding-regular px-padding-bold rounded-interactive-default inline-flex items-center justify-center border'>
-        hi
-      </p>
-      <p className='hover:hovered hover:bg-[color-mix()]  text-callout-key danger text-data-base   size-lg py-padding-regular px-padding-bold border border-amber-200'>
-        asdasda
-      </p>
       <Button label='label' size={'sm'} peak={true} style={'surface'} />
-      <Button label='hover' size={'sm'} peak={false} style={'surface'} />
-      <Button label='label' size={'sm'} peak={true} style={'accent'} className='hover:hoverd' />
-      <Button label='label' size={'md'} peak={true} style={'accent'} />
+      <Button label='label' size={'md'} peak={false} style={'data'} />
       <Button label='label' size={'lg'} peak={true} style={'accent'} />
+      <Button label='label' size={'lg'} peak={true} style={'outline'} />
+      <Button label='label' size={'lg'} peak={false} style={'outline'} />
+      <TextBtn label='label' style={'data'} />
+      <button className=' p-padding-regular bg-surface rounded-interactive-default'>
+        <ContentIcon className='size-5' />
+      </button>
+      <IconBtn size={'sm'} style={'surface'} peak={true} />
+      <IconBtn size={'sm'} style={'surface'} peak={false} />
+      <IconBtn size={'sm'} style={'outline'} peak={true} />
+      <IconBtn size={'sm'} style={'outline'} peak={false} />
+      <IconBtn size={'sm'} style={'surface'} peak={false} />
+      <IconBtn size={'sm'} style={'accent'} peak={false} />
+      <IconBtn size={'sm'} style={'accent'} peak={true} />
+      <IconBtn size={'sm'} style={'accent'} peak={false} />
+      <IconBtn size={'sm'} style={'data'} peak={true} />
+      <IconBtn size={'sm'} style={'data'} peak={false} />
     </div>
   );
 };
