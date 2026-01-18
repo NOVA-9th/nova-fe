2ecca27cac8a339c570b8e5a4e1db312eafa367a
wldsmdl7 <wldmsdl5835@gmail.com>
2026-01-18 18:03:14 +0900

    💄 style: toggle 컴포넌트 padding 값 디자인 시스템 활용

--- nova-app/src/shared/ui/action/ToggleBtn.tsx
+++ nova-app/src/shared/ui/action/ToggleBtn.tsx
@@ -1,47 +1,50 @@
 import { cva } from 'class-variance-authority';
-const ToggleBtnVariants = cva('flex items-center justify-center rounded-interactive-default', {
-  variants: {
-    size: {
-      lg: 'py-2.5 px-3 gap-1.5 typo-body-base',
-      md: 'py-2.5 px-3 gap-1.5 typo-callout-base',
+const ToggleBtnVariants = cva(
+  'flex items-center justify-center rounded-interactive-default px-padding-regular py-padding-medium gap-1.5',
+  {
+    variants: {
+      size: {
+        lg: 'size-lg typo-body-base',
+        md: 'size-md typo-callout-base',
+      },
+
+      variant: {
+        surface: '',
+        outline: '',
+      },
+
+      selected: {
+        true: '',
+        false: '',
+      },
     },
 
-    variant: {
-      surface: '',
-      outline: '',
-    },
-
-    selected: {
-      true: '',
-      false: '',
-    },
+    compoundVariants: [
+      // surface + selected 조합
+      {
+        variant: 'surface',
+        selected: true,
+        class: 'bg-surface text-base',
+      },
+      {
+        variant: 'surface',
+        selected: false,
+        class: 'text-optional ',
+      },
+      // outline + selected 조합
+      {
+        variant: 'outline',
+        selected: true,
+        class: 'bg-surface text-base border border-selected',
+      },
+      {
+        variant: 'outline',
+        selected: false,
+        class: 'text-optional border border-outline ',
+      },
+    ],
   },
-
-  compoundVariants: [
-    // surface + selected 조합
-    {
-      variant: 'surface',
-      selected: true,
-      class: 'bg-surface text-base',
-    },
-    {
-      variant: 'surface',
-      selected: false,
-      class: 'text-optional ',
-    },
-    // outline + selected 조합
-    {
-      variant: 'outline',
-      selected: true,
-      class: 'bg-surface text-base border border-selected',
-    },
-    {
-      variant: 'outline',
-      selected: false,
-      class: 'text-optional border border-outline ',
-    },
-  ],
-});
+);
 
 interface ToggleBtnProps {
   size: 'md' | 'lg';
