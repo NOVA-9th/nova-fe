528bb58011e5255221b5bd784668ec53b368ba3f
kim <jskim6335@naver.com>
2026-01-22 01:46:30 +0900

    :lipstick: styles: 페이지 레이아웃 작업 완료

---
+++ nova-app/src/app/(protected)/layout.tsx
@@ -0,0 +1,14 @@
+
+
+const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex flex-col w-screen h-screen justify-start items-center bg-alternative">
+            <div className="h-19 w-full bg-white"></div>
+            <div className="flex flex-1 justify-center items-center w-full">
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default ProtectedLayout;
---
+++ nova-app/src/app/(protected)/saved/page.tsx
@@ -0,0 +1,23 @@
+import SectionHeader from "@/shared/ui/content/SectionHeader/SectionHeader";
+import { KeywordCard } from "@/features/saved/ui/keywordCard";
+
+
+const SavedPage = () => {
+    return (
+        <div className="flex flex-col w-full h-full justify-start items-center bg-alternative">
+            <div className="h-19 w-full bg-alternative"></div>
+            <div className="flex flex-col w-full h-full justify-start items-center bg-alternative">
+                <div className="flex w-full h-full justify-center items-center px-5 gap-4">
+                    <div className="flex-2 h-full bg-red-500">
+                        <KeywordCard/>
+                    </div>
+                    <div className="flex-1 h-full bg-blue-500">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SavedPage;
