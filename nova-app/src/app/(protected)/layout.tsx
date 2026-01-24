936ee2ad467af3d7197b341b0072e488ebc23c92
정찬원 <jcw0522@gachon.ac.kr>
2026-01-24 16:07:30 +0900

    💄 style: 페이지 레이아웃 max-width 설정

--- nova-app/src/app/(protected)/layout.tsx
+++ nova-app/src/app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import { HeaderBar, SideBar } from '@/widgets/layouts';
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-dvh grid grid-rows-[auto_1fr]'>
+    <div className='h-dvh grid grid-rows-[auto_1fr] border-l border-r border-outline max-w-360 mx-auto'>
       <HeaderBar />
       <div className='grid grid-cols-[320px_1fr]'>
         <SideBar />
