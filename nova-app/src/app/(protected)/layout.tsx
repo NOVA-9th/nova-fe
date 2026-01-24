a47ba6eed796fc0208d447838d2de298ca3a9c6c
정찬원 <jcw0522@gachon.ac.kr>
2026-01-24 13:41:34 +0900

    💄 style: 페이지 메인 레이아웃 스타일 설정

--- nova-app/src/app/(protected)/layout.tsx
+++ nova-app/src/app/(protected)/layout.tsx
@@ -6,7 +6,9 @@ const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
       <HeaderBar />
       <div className='grid grid-cols-[320px_1fr]'>
         <SideBar />
-        <main className='overflow-auto p-4 bg-alternative'>{children}</main>
+        <main className='bg-base p-4 bg-alternative rounded-static-frame mr-4 mb-4'>
+          {children}
+        </main>
       </div>
     </div>
   );
