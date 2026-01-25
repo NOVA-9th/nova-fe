8a857546ba101fe7e98938a97ae869e73f3594b0
CHANWON <jcw0522@gachon.ac.kr>
2026-01-25 16:30:40 +0900

    ✨ feat: 피드 페이지에서만 헤더 검색 input이 보이도록 조건 추가

--- nova-app/src/widgets/layouts/header/HeaderBar.tsx
+++ nova-app/src/widgets/layouts/header/HeaderBar.tsx
@@ -3,9 +3,13 @@
 import { TextInput } from '@/shared/ui';
 import { Lightbulb, Search } from 'lucide-react';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 const HeaderBar = () => {
   const [keyword, setKeyword] = useState('');
+  const pathname = usePathname();
+
+  const isHome = pathname === '/';
 
   return (
     <header className='p-4 px-5 flex justify-between items-center h-19'>
@@ -13,14 +17,17 @@ const HeaderBar = () => {
         <Lightbulb size={24} />
         <span className='text-lg font-semibold'>nova</span>
       </button>
-      <TextInput
-        size='lg'
-        value={keyword}
-        onChange={setKeyword}
-        icon={Search}
-        placeholder='아티클 및 트렌드를 검색해보세요'
-        className='w-100'
-      />
+
+      {isHome && (
+        <TextInput
+          size='lg'
+          value={keyword}
+          onChange={setKeyword}
+          icon={Search}
+          placeholder='아티클 및 트렌드를 검색해보세요'
+          className='w-100'
+        />
+      )}
     </header>
   );
 };
