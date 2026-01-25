ef1391353ae720c7e2c456cd4775bbc1dd1c5665
wldsmdl7 <wldmsdl5835@gmail.com>
2026-01-26 03:13:36 +0900

    💄 style: 온보딩 페이지 내 stepper 및 header 구현

--- nova-app/src/app/(pages)/onboarding/page.tsx
+++ nova-app/src/app/(pages)/onboarding/page.tsx
@@ -1,12 +1,32 @@
-import Stepper from '@/features/onboarding/ui/stepper';
+'use client';
+
+import { useState } from 'react';
+
+import Stepper from '@/features/onboarding/ui/Stepper';
+import { Header } from '@/shared/ui';
+import { STEP_ITEMS } from '@/features/onboarding/data/StepItems';
+import StepCard from '@/features/onboarding/ui/StepCard';
+
+const STEPS = [1, 2, 3, 4] as const;
+type Step = (typeof STEPS)[number];
 
 const OnboardingPage = () => {
+  const [currentStep, setCurrentStep] = useState<Step>(1);
+
+  const currentItem = STEP_ITEMS[currentStep];
+  const StepComponent = currentItem.Component;
+
   return (
-    <div className='flex flex-col items-center justify-center gap-4'>
-      <Stepper currentStep={1} labels={['전공 분야', '관심 분야', '기술 역량', '관심 키워드']} />
-      <Stepper currentStep={2} labels={['전공 분야', '관심 분야', '기술 역량', '관심 키워드']} />
-      <Stepper currentStep={3} labels={['전공 분야', '관심 분야', '기술 역량', '관심 키워드']} />
-      <Stepper currentStep={4} labels={['전공 분야', '관심 분야', '기술 역량', '관심 키워드']} />
+    <div className='flex items-center justify-center min-h-screen'>
+      <div className='flex flex-col w-160 bg-base rounded-static-frame min-h-94 max-h-123.5 gap-5 p-5'>
+        <Stepper
+          currentStep={currentStep}
+          labels={['전공 분야', '관심 분야', '기술 역량', '관심 키워드']}
+        />
+
+        <Header size='lg' label={currentItem.label} description={currentItem.description} />
+        <StepCard></StepCard>
+      </div>
     </div>
   );
 };
---
+++ nova-app/src/features/onboarding/data/StepItems.ts
@@ -0,0 +1,27 @@
+import InterestCard from '../ui/InterestCard';
+import InterestKeywordCard from '../ui/InterestKeywordCard';
+import MajorCard from '../ui/MajorCard';
+import SkillCard from '../ui/SkillCard';
+
+export const STEP_ITEMS = {
+  1: {
+    label: '전공 분야 선택',
+    description: '현재 학교나 직장에서의 전공 분야를 선택해주세요',
+    Component: MajorCard,
+  },
+  2: {
+    label: '관심 분야 선택',
+    description: 'IT 직군 내 관심있는 분야를 선택해주세요 (최대 2개)',
+    Component: InterestCard,
+  },
+  3: {
+    label: '기술 역량 선택',
+    description: 'IT 직군 내에서의 난이도와 기술 역량을 선택해주세요',
+    Component: SkillCard,
+  },
+  4: {
+    label: '관심 키워드 선택',
+    description: 'IT 직군 내에서의 난이도와 기술 역량을 선택해주세요',
+    Component: InterestKeywordCard,
+  },
+} as const;
