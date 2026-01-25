cb947bdc8bb41fbb3e469b0823f48060fc01b864
wldsmdl7 <wldmsdl5835@gmail.com>
2026-01-26 02:34:01 +0900

    ♻️ refactor: 이삭 코드 리뷰 반영

--- nova-app/next.config.ts
+++ nova-app/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // dev(turbopack)용
   turbopack: {
     rules: {
       '*.svg': {
@@ -9,15 +8,6 @@ const nextConfig: NextConfig = {
         as: '*.js',
       },
     },
-  },
-
-  // build(webpack)용
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    });
-    return config;
   },
 };
 
