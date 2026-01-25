7e77ac27face79085cc1ad016b7f8b677a14d944
wldsmdl7 <wldmsdl5835@gmail.com>
2026-01-26 00:53:30 +0900

    📦 build: svgr 라이브러리 설치 및 설정

--- nova-app/next.config.ts
+++ nova-app/next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // dev(turbopack)용
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+
+  // build(webpack)용
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
