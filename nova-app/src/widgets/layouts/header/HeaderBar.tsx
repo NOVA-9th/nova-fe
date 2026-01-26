ebad1c142f72ac2472e6311e7d663df5c352cb81
정찬원 <jcw0522@gachon.ac.kr>
2026-01-26 18:27:22 +0900

    ✨ feat: 사이드탭 호버 색상 추가 & 헤더 아이콘 클릭 시 홈으로 이동

--- nova-app/src/widgets/layouts/header/HeaderBar.tsx
+++ nova-app/src/widgets/layouts/header/HeaderBar.tsx
@@ -3,17 +3,22 @@
 import { TextInput } from '@/shared/ui';
 import { Lightbulb, Search } from 'lucide-react';
 import { useState } from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 const HeaderBar = () => {
   const [keyword, setKeyword] = useState('');
   const pathname = usePathname();
+  const router = useRouter(); // ✅ 추가
 
   const isHome = pathname === '/';
 
   return (
     <header className='p-4 px-5 flex justify-between items-center h-19'>
-      <button type='button' className='flex items-center gap-1.5 text-base-color'>
+      <button
+        type='button'
+        className='flex items-center gap-1.5 text-base-color'
+        onClick={() => router.push('/')}
+      >
         <Lightbulb size={24} />
         <span className='text-lg font-semibold'>nova</span>
       </button>
