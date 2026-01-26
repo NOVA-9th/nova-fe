816693d87748ce672feded044d0b1ff39949e38c
wldsmdl7 <wldmsdl5835@gmail.com>
2026-01-26 12:05:10 +0900

    💄 style: 기술 역량 컴포넌트 구현

---
+++ nova-app/src/features/onboarding/ui/SkillCard.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { StepComponentProps } from '@/features/onboarding/types/StepItem';
+import { ToggleButton } from '@/shared/ui';
+import { useEffect, useState } from 'react';
+import { SKILL_OPTIONS } from '../data/SkillOptions';
+
+const SkillCard = ({ onValidChange }: StepComponentProps) => {
+  const [selected, setSelected] = useState<string | null>(null);
+
+  useEffect(() => {
+    onValidChange(!!selected);
+  }, [selected, onValidChange]);
+
+  const toggleItem = (text: string) => {
+    setSelected((prev) => (prev === text ? null : text));
+  };
+
+  return (
+    <div className='w-150 h-24.5 flex flex-wrap gap-2.5'>
+      {SKILL_OPTIONS.map((text) => (
+        <ToggleButton
+          size='lg'
+          key={text}
+          text={text}
+          variant='outline'
+          selected={selected === text}
+          onClick={() => toggleItem(text)}
+          className='w-73.5 h-11'
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SkillCard;
