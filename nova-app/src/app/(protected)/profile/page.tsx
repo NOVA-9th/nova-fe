1ddda19b65c88ac6eef44b18ec8bce389862a4eb
jskim6335-plaid <jskim6335@plaid.ai.kr>
2026-01-29 02:49:49 +0900

    :lipstick: style: UserInfoSection 컴포넌트 구현 완료

--- nova-app/src/app/(protected)/profile/page.tsx
+++ nova-app/src/app/(protected)/profile/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+import { UserInfoSection } from '@/features/profile/UserInfoSection';
 import { SectionHeader } from '@/shared/ui';
 import { User } from 'lucide-react';
+import { useState } from 'react';
 
 const ProfilePage = () => {
+  const [value, setValue] = useState('김진성');
+
   return (
     <>
       <header className='flex h-15 p-4 w-full rounded-static-frame bg-alternative'>
@@ -9,7 +14,15 @@ const ProfilePage = () => {
       </header>
       <div className='flex w-full h-full justify-start items-center bg-alternative'>
         <div className='flex w-full h-full justify-center items-center px-5 pb-5 gap-4'>
-          <div className='flex flex-col justify-start items-start w-full h-full gap-4'></div>
+          <div className='flex flex-col justify-start items-start w-full h-full gap-4'>
+            <UserInfoSection
+              name='이름'
+              description='디자인 전공 | 프론트엔드'
+              image='/test.png'
+              value={value}
+              setValue={setValue}
+            />
+          </div>
         </div>
       </div>
     </>
