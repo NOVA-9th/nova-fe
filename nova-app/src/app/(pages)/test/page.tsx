b1c08437bdf350e49a40385e597de97018472ce0
정찬원 <jcw0522@gachon.ac.kr>
2026-01-29 15:55:06 +0900

    💄 style: 토스트 메세지 공용 컴포넌트 구현

--- nova-app/src/app/(pages)/test/page.tsx
+++ nova-app/src/app/(pages)/test/page.tsx
@@ -1,7 +1,26 @@
+'use client';
+
+import { showToast } from '@/shared/utils/toast';
+
 const testPage = () => {
   return (
     <div className='bg-base flex min-h-dvh flex-col items-center justify-center gap-5 rounded-interactive-default px-4 text-xl'>
-      안녕하세요
+      <button
+        onClick={() => {
+          showToast.success('아티클이 저장되었습니다!');
+        }}
+        className='bg-green-500 text-white p-2'
+      >
+        토스트 띄우기
+      </button>
+      <button
+        onClick={() => {
+          showToast.error('아티클이 더이상 노출되지 않습니다!');
+        }}
+        className='bg-red-500 text-white p-2'
+      >
+        토스트 띄우기
+      </button>
     </div>
   );
 };
---
+++ nova-app/src/shared/ui/toast/Toast.tsx
@@ -0,0 +1,52 @@
+import { MessageCircleWarning, CircleCheck } from 'lucide-react';
+import { Toaster } from 'react-hot-toast';
+
+const Toast = () => {
+  return (
+    <Toaster
+      position='top-center'
+      toastOptions={{
+        duration: 3000,
+        success: {
+          icon: <CircleCheck className='h-6 w-6 text-[#00BD7599]' />,
+          style: {
+            background: '#FFFFFF',
+            color: '#00BD75',
+            borderRadius: '8px',
+            border: '2px solid rgba(0, 189, 117, 0.3)',
+            padding: '12px 16px',
+            fontSize: '16px',
+            fontWeight: '400',
+            lineHeight: '16px',
+            whiteSpace: 'nowrap',
+            wordBreak: 'keep-all',
+            minWidth: '200px',
+          },
+        },
+        error: {
+          icon: <MessageCircleWarning className='h-6 w-6 text-[#F04C5A]' />,
+          style: {
+            background: '#FFFFFF',
+            color: '#F04C5A',
+            borderRadius: '8px',
+            border: '2px solid #F04C5A80',
+            padding: '12px 16px',
+            fontSize: '16px',
+            fontWeight: '400',
+            lineHeight: '16px',
+            whiteSpace: 'nowrap',
+            wordBreak: 'keep-all',
+            minWidth: '200px',
+          },
+        },
+      }}
+      containerStyle={{
+        top: '60px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      }}
+    />
+  );
+};
+
+export default Toast;
