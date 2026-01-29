1d1604cc553fe71d47af0948099f00f0e7fd5812
정찬원 <jcw0522@gachon.ac.kr>
2026-01-30 00:50:49 +0900

    ♻️ refactor: 메인 화면 모바일 레이아웃 적용

--- nova-app/src/app/(protected)/layout.tsx
+++ nova-app/src/app/(protected)/layout.tsx
@@ -2,14 +2,12 @@ import { HeaderBar, SideBar } from '@/widgets/layouts';
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='mx-auto flex h-dvh max-w-360 flex-col overflow-hidden border-l border-r border-outline'>
+    <div className='mx-auto flex h-dvh max-w-360 flex-col overflow-hidden sm:border-l sm:border-r border-outline'>
       <HeaderBar />
 
       <div className='flex min-h-0 flex-1'>
-        <div className='hidden w-80 sm:block'>
-          <SideBar />
-        </div>
-        <main className='min-w-0 flex-1 overflow-y-auto bg-alternative sm:mr-4 sm:mb-4 sm:rounded-static-frame sm:border border-outline no-scrollbar'>
+        <SideBar />
+        <main className='min-w-0 flex-1 overflow-y-auto bg-alternative max-sm:mx-4 sm:mr-4 mb-4 rounded-static-frame border border-outline no-scrollbar'>
           {children}
         </main>
       </div>
