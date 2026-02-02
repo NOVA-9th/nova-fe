2c7b11297890740c9810a0c1f8dcf1faf92e040e
wldsmdl7 <wldmsdl5835@gmail.com>
2026-02-02 16:01:24 +0900

    💄 style: 로고 패딩 값 및 크기 수정

--- nova-app/src/features/login/ui/LoginForm.tsx
+++ nova-app/src/features/login/ui/LoginForm.tsx
@@ -5,9 +5,11 @@ import { LoginButton } from './LoginButton';
 export const LoginForm = () => {
   return (
     <main className='w-full h-107.5 rounded-static-frame p-5 gap-5 flex flex-col items-center justify-center bg-base'>
-      <div className='flex gap-3.5 items-center justify-center py-5 px-1 w-full h-22 sm:w-150'>
-        <Logo width={60} height={60} />
-        <NovaLabel width={100} height={26} />
+      <div className='flex gap-1.5 items-center justify-center pt-7 px-1 w-full h-22 sm:w-150'>
+        <Logo width={50} height={50} />
+        <span className='pr-5'>
+          <NovaLabel width={100} height={26} />
+        </span>
       </div>
       <Header
         size='lg'
@@ -15,7 +17,7 @@ export const LoginForm = () => {
         description='AI 요약, 트렌드 분석, 맞춤 추천까지'
         className='items-center'
       />
-      <div className='flex flex-col gap-2.5 w-full justify-center items-center sm:w-150'>
+      <div className='flex flex-col gap-3 w-full justify-center items-center sm:w-150'>
         <LoginButton type='google' />
         <LoginButton type='kakao' />
       </div>
