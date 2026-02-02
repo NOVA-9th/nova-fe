4b572c9254f3d26318d4f3d2c2f742570c365974
wldsmdl7 <wldmsdl5835@gmail.com>
2026-02-02 17:54:48 +0900

    ♻️ refactor: 누누 코드리뷰 반영

--- nova-app/src/app/(pages)/layout.tsx
+++ nova-app/src/app/(pages)/layout.tsx
@@ -1,6 +1,6 @@
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex items-center justify-center min-h-screen mx-auto bg-white-full'>
+    <div className='flex items-center justify-center min-h-screen mx-auto bg-white-full px-4'>
       {children}
     </div>
   );
--- nova-app/src/features/login/ui/LoginForm.tsx
+++ nova-app/src/features/login/ui/LoginForm.tsx
@@ -4,7 +4,7 @@ import { LoginButton } from './LoginButton';
 
 export const LoginForm = () => {
   return (
-    <main className='w-full h-107.5 rounded-static-frame p-5 gap-5 flex flex-col items-center justify-center bg-base'>
+    <main className='w-full h-107.5 rounded-static-frame p-5 gap-5 flex flex-col items-center justify-center bg-base max-w-160'>
       <div className='flex gap-1.5 items-center justify-center pt-7 px-1 w-full h-22 sm:w-150'>
         <Logo width={50} height={50} />
         <NovaLabel width={100} height={26} />
