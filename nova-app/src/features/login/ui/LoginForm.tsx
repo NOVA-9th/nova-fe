6525bb61358a1bf4c05202b466cd117b2f4dc09f
wldsmdl7 <wldmsdl5835@gmail.com>
2026-02-06 18:25:31 +0900

    💄 style: 로그인 패딩 값 및 크기 수정

--- nova-app/src/features/login/ui/LoginForm.tsx
+++ nova-app/src/features/login/ui/LoginForm.tsx
@@ -4,18 +4,22 @@ import { LoginButton } from './LoginButton';
 
 export const LoginForm = () => {
   return (
-    <main className='w-full h-107.5 rounded-static-frame p-5 gap-5 flex flex-col items-center justify-center bg-base max-w-160'>
-      <div className='flex gap-1.5 items-center justify-center pt-7 px-1 w-full h-22 sm:w-150'>
-        <Logo width={50} height={50} />
-        <NovaLabel width={100} height={26} />
+    <main className='w-full h-107.5 rounded-static-frame p-5 gap-5 flex flex-col items-center bg-base max-w-160'>
+      <div className='flex flex-col pt-5 gap-1.5 items-center'>
+        <div className='flex flex-row w-49.5 h-15 justify-between items-center'>
+          <Logo width={60} height={60} />
+          <span className='pr-5'>
+            <NovaLabel width={108} height={30} />
+          </span>
+        </div>
+        <Header
+          size='md'
+          label='IT 취준생을 위한 AI 큐레이션 플랫폼'
+          description='AI 요약, 트렌드 분석, 맞춤 추천까지'
+          className='items-center'
+        />
       </div>
-      <Header
-        size='lg'
-        label='IT 취준생을 위한 AI 큐레이션 플랫폼'
-        description='AI 요약, 트렌드 분석, 맞춤 추천까지'
-        className='items-center'
-      />
-      <div className='flex flex-col gap-3 w-full justify-center items-center sm:w-150'>
+      <div className='flex flex-col gap-2.5 w-full px-10 py-7.75 justify-center items-center sm:w-150'>
         <LoginButton type='google' />
         <LoginButton type='kakao' />
       </div>
