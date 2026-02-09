68420d6b69eb5c450d4e022ee4597b08597a8780
JeonSuna <jsa020910@gmail.com>
2026-02-09 13:04:11 +0900

    ✅ test: rebase test

--- nova-app/src/features/trend/ui/BarChart.tsx
+++ nova-app/src/features/trend/ui/BarChart.tsx
@@ -5,6 +5,7 @@ import { Bar } from 'react-chartjs-2';
 
 export const BarChart = () => {
   const chartData = useCategoryRank();
+  console.log('');
   return (
     <div className='h-90'>
       <Bar data={chartData} options={categoryRankOptions} />
