c2363fced07a009fe12d3c8d0bb74555cfcb4d1a
wldsmdl7 <wldmsdl5835@gmail.com>
2026-02-12 18:53:21 +0900

    ♻️ refactor: 피드 페이지 필터 섹션 전체 최적화

--- nova-app/src/shared/ui/action/TextButton.tsx
+++ nova-app/src/shared/ui/action/TextButton.tsx
@@ -2,7 +2,7 @@ import { TextBtnVariants } from '@/shared/ui/styles/textButton.styles';
 import { cn } from '@/shared/utils/cn';
 import { VariantProps } from 'class-variance-authority';
 import { LucideIcon } from 'lucide-react';
-import React from 'react';
+import React, { memo } from 'react';
 
 interface TextButtonProps extends VariantProps<typeof TextBtnVariants> {
   onClick?: () => void;
@@ -12,32 +12,26 @@ interface TextButtonProps extends VariantProps<typeof TextBtnVariants> {
   rightIcon?: LucideIcon;
 }
 
-export const TextButton = ({
-  label,
-  onClick,
-  className,
-  size,
-  style,
-  leftIcon,
-  rightIcon,
-}: TextButtonProps) => {
-  return (
-    <button onClick={onClick} className={cn(TextBtnVariants({ size, style }), className)}>
-      {leftIcon && (
-        <span>
-          {React.createElement(leftIcon, {
-            size: size === 'sm' ? 12 : size === 'lg' ? 16 : 14,
-          })}
-        </span>
-      )}
-      {label}
-      {rightIcon && (
-        <span>
-          {React.createElement(rightIcon, {
-            size: size === 'sm' ? 12 : size === 'lg' ? 16 : 14,
-          })}
-        </span>
-      )}
-    </button>
-  );
-};
+export const TextButton = memo(
+  ({ label, onClick, className, size, style, leftIcon, rightIcon }: TextButtonProps) => {
+    return (
+      <button onClick={onClick} className={cn(TextBtnVariants({ size, style }), className)}>
+        {leftIcon && (
+          <span>
+            {React.createElement(leftIcon, {
+              size: size === 'sm' ? 12 : size === 'lg' ? 16 : 14,
+            })}
+          </span>
+        )}
+        {label}
+        {rightIcon && (
+          <span>
+            {React.createElement(rightIcon, {
+              size: size === 'sm' ? 12 : size === 'lg' ? 16 : 14,
+            })}
+          </span>
+        )}
+      </button>
+    );
+  },
+);
